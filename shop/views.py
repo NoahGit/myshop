7548660aa39c2574747127759e820d74e5527292
noah <gaoyong@xatu.edu.cn>
2020-03-17 20:41:16 +0800

    20200317-7.2.5创建购物车视图-bootstrap修改页面样式，放弃使用base.css-v1

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 from .forms import UserRegistrationForm
 
 
