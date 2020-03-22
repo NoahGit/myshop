a56258ea0c2a0bed5a008e92e2c8e34cc99a1315
noah <gaoyong@xatu.edu.cn>
2020-03-22 22:42:31 +0800

    20200322-8.2将订单导出为CSV文件-v1

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+import csv
+import datetime
+from django.http import HttpResponse
 
 
 # Register your models here.
+def export_to_csv(modeladmin, request, queryset):
+    # 属性的访问权限：共有、受保护、私有。
+    # Python中并没有真正的私有化支持，但是可以使用下划线，完成伪私有的效果，例如下面的'_meta'
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # Write a first row with header information用标题信息写第一行
+    writer.writerow([field.verbose_name for field in fields])
+    # Write data rows写数据行
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
+
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -14,3 +43,4 @@ class OrderAdmin(admin.ModelAdmin):
                     'address', 'postal_code', 'city', 'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
