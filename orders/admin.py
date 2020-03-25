bd2ab8b819da2adea21f1be138d600df3992b703
noah <gaoyong@xatu.edu.cn>
2020-03-25 17:49:24 +0800

    20200325-8.3利用自定义视图扩展管理站点-v1

--- orders/admin.py
+++ orders/admin.py
@@ -3,9 +3,15 @@ from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 
 # Register your models here.
+def order_detail(obj):
+    return mark_safe('<a href="{}">View</a>'.format(reverse('orders:admin_order_detail', args=[obj.id])))
+
+
 def export_to_csv(modeladmin, request, queryset):
     # 属性的访问权限：共有、受保护、私有。
     # Python中并没有真正的私有化支持，但是可以使用下划线，完成伪私有的效果，例如下面的'_meta'
@@ -40,7 +46,7 @@ class OrderItemInline(admin.TabularInline):
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-                    'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+                    'address', 'postal_code', 'city', 'paid', 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
